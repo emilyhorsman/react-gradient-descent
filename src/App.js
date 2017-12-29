3b0ffed0fd26232e038637d40ff0beaa7a42b4c7
Emily Horsman <me@emilyhorsman.com>
2017-12-29 14:56:03 -0500

    Add some explainin'

--- src/App.js
+++ src/App.js
@@ -112,48 +112,96 @@ class App extends Component {
 
 		return (
 			<span className="App">
-				<span>
-					<button onClick={this.handleTogglePlaying}>
-						{isPlaying ? 'Pause' : 'Play'}
-					</button>
+				<span className="Controls">
+					<p>
+					We want to draw a straight line through the <span style={{ color: 'tomato' }}>red data points</span> below, but not just any line!
+					In the future, we might be given a new <span style={{ color: 'tomato' }}>red data point</span> without knowing exactly where to put it.
+					We want to be able to put it on the straight line as the best prediction of where it should go as a straight line can offer.
+					The gradient descent algorithm lets us do this.
+					We start with a random initial assumption of the coefficients of our line.
+					Then we determine how far off this line is from all our <span style={{ color: 'tomato' }}>red data points</span> and call this our loss.
+					In one iteration, we adjust the coefficients so that our line takes a small step towards reducing the loss we just calculated.
+					Then we keep repeating!
+					This is called a <b>gradient descent algorithm</b> because we take the derivative of the function that computes our loss and look at which direction we can descend to reduce our loss.
+					This is ultimately an optimization problem where we are trying to minimize the loss as much as possible, best fitting our <span style={{ color: 'tomato' }}>red data points</span>.
+					</p>
 
-					<button onClick={this.handleReset}>
-						Reset
-					</button>
+					<span>
+						<button onClick={this.handleTogglePlaying}>
+							{isPlaying ? 'Pause' : 'Play'}
+						</button>
 
-					<button onClick={this.handleNewData}>
-						New Data!
-					</button>
+						<p>
+							Hitting Play will start running the optimizer!
+							It will keep computing iterations that have our <span style={{ color: 'magenta' }}>magenta line</span> get closer to fitting our <span style={{ color: 'tomato' }}>red data points</span> until you hit Pause.
+						</p>
+					</span>
 
-					<label>
-						Learning Rate:
-						<input
-							type="number"
-							step={0.001}
-							value={input.learningRate}
-							onChange={this.handleChange.bind(this, 'learningRate')}
-						/>
-					</label>
+					<span>
+						<button onClick={this.handleReset}>
+							Reset
+						</button>
 
-					<label>
-						Learned Weights:
+						<p>
+							This will wipe out the iterations we’ve computed so far, but won’t touch your carefully chosen learning rate.
+						</p>
+					</span>
 
-						<input
-							type="number"
-							step={0.25}
-							value={input.weightB}
-							onChange={this.handleChange.bind(this, 'weightB')}
-							disabled={isPlaying}
-						/>
+					<span>
+						<button onClick={this.handleNewData}>
+							New Data!
+						</button>
 
-						<input
-							type="number"
-							step={0.25}
-							value={input.weightX}
-							onChange={this.handleChange.bind(this, 'weightX')}
-							disabled={isPlaying}
-						/>
-					</label>
+						<p>
+							Maybe you’re tired of these <span style={{ color: 'tomato' }}>red data points</span> and want to exchange them for some new ones.
+						</p>
+					</span>
+
+					<span>
+						<label>
+							Learning Rate:
+							<input
+								type="number"
+								step={0.001}
+								value={input.learningRate}
+								onChange={this.handleChange.bind(this, 'learningRate')}
+							/>
+						</label>
+
+						<p>
+							This value tells our optimizer how large a step to take each iteration.
+							If the graph on the left blows up and makes no sense at all, the optimizer probably descended too far and overshot back up a hill — lower the value.
+							If the <span style={{ color: 'magenta' }}>magenta line</span> takes too long to fit the points — raise this value.
+							If the <span stlye={{ color: 'magenta' }}>magenta line</span> changes its slope but doesn’t seem to change its offset — raise this value.
+						</p>
+					</span>
+
+					<span>
+						<label>
+							Learned Weights:
+
+							<input
+								type="number"
+								step={0.25}
+								value={input.weightB}
+								onChange={this.handleChange.bind(this, 'weightB')}
+								disabled={isPlaying}
+							/>
+
+							<input
+								type="number"
+								step={0.25}
+								value={input.weightX}
+								onChange={this.handleChange.bind(this, 'weightX')}
+								disabled={isPlaying}
+							/>
+						</label>
+
+						<p>
+							These are the coefficients of our <span style={{ color: 'magenta' }}>magenta line</span>!
+							They will be randomly selected at first, and then will change as you run the optimizer.
+						</p>
+					</span>
 				</span>
 
 				<span className="Charts">
